19a3302bc937dee6e74e93684935cf4df5143f35
rakib-utsho <rakibutsho1920@gmail.com>
2026-01-10 09:45:05 +0600

    problem-1010

---
+++ BEGINNER/problem-1010/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func product() (int, int, float64) {
+	var a, b int
+	var c float64
+
+	fmt.Scanf("%d %d %f\n", &a, &b, &c)
+	return a, b, c
+}
+func productValue(a int, b int, c float64) float64 {
+	var total = float64(b) * c
+
+	return total
+}
+
+func totalValue(a float64, b float64) float64 {
+	var total = a + b
+
+	return total
+
+}
+
+func main() {
+	a, b, c := product()
+	total1 := productValue(a, b, c)
+
+	d, e, f := product()
+	total2 := productValue(d, e, f)
+
+	result := totalValue(total1, total2)
+
+	fmt.Printf("VALOR A PAGAR: R$ %.2f\n", result)
+
+}
