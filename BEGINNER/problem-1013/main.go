fc3ee9728db7c233e3248a1ff61f192ced8da825
rakib-utsho <rakibutsho1920@gmail.com>
2026-01-10 17:07:57 +0600

    problem-1013

---
+++ BEGINNER/problem-1013/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+	var a, b, c int
+
+	fmt.Scanf("%d %d %d\n", &a, &b, &c)
+
+	var maiorAB = (a + b + abs(a-b)) / 2
+
+	var gretestABC = (maiorAB + c + abs(maiorAB-c)) / 2
+
+	fmt.Println(gretestABC, "eh o maior")
+
+}
