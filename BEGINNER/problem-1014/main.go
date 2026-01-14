79892bda77f2abe45165a0ce85f36b9b46cdb622
rakib-utsho <rakibutsho1920@gmail.com>
2026-01-14 11:11:17 +0600

    problem-1014

---
+++ BEGINNER/problem-1014/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var X int
+	var Y float64
+
+	fmt.Scanf("%d\n", &X)
+	fmt.Scanf("%f\n", &Y)
+
+	var consumption float64 = float64(X) / Y
+
+	fmt.Printf("%.3f km/l\n", consumption)
+}
