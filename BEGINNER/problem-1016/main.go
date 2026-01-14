486c4b409a09e9e1601f58642964727143f59b04
rakib-utsho <rakibutsho1920@gmail.com>
2026-01-14 11:37:20 +0600

    problem-1016

---
+++ BEGINNER/problem-1016/main.go
@@ -0,0 +1,15 @@
+// 1016 - Distance
+
+package main
+
+import "fmt"
+
+func main() {
+	var distance int
+
+	fmt.Scanln(&distance)
+
+	distanceTime := distance * 2
+
+	fmt.Println(distanceTime, "minutos")
+}
